22c588db486a5fac808ea280a6b27dbc5f7790db
Yosuke Yamashita <yamman.sb@gmail.com>
2021-06-11 11:21:03 +0900

    [clean]コメントアウトしたコードを削除

--- lib/src/main/java/junit5/Supermarket/impl/ItemNumImpl.java
+++ lib/src/main/java/junit5/Supermarket/impl/ItemNumImpl.java
@@ -19,7 +19,6 @@ public class ItemNumImpl implements ItemNum {
   @Override
   public int calEachDiscount(List<ItemInfo> itemInfos) {
     int discount = 0;
-    // りンゴ
     for (ItemInfo itemInfo : itemInfos) {
       if (itemInfo.getItem().getId() == Item.APPLE.getId()) {
         discount = eventDiscount.appleEventDiscount(itemInfo.getItem().getId(), itemInfo.getSumQuantity());
@@ -28,46 +27,3 @@ public class ItemNumImpl implements ItemNum {
     return discount;
   }
 }
-
-//   // @Override
-//   // public static List<ItemInfo> calEachDiscount(List<ItemInfo> items){
-//   //   // 課題4のセールを行う
-//   //   appleEventDiscountSum = EventDiscount.appleEventDiscount();
-//   //   // 課題5のセールを行う
-//   //   tenItemNumDiscoutSum = EventDiscount.tenItemNum();
-//   //   // 課題6のセールを行う
-//   //   EventDiscount.tabaccoEventDiscount();
-//   //   // 課題7のセールを行う
-
-//   //   // それぞれのセールを比較する
-//   // };
-
-
-// }
-
-
-// //
-//   // private int appleNum;
-//   // private int three_Set_Num;
-
-//   // public ItemNum(int appleNum,int three_Set_Num)
-//   // {
-//   //   this.appleNum = appleNum;
-//   //   this.three_Set_Num = three_Set_Num;
-//   // }
-
-//   // public int get_apple_Num(){
-//   //   int unit_apple_Price = 0;
-//   //   unit_apple_Price = Item.APPLE.price();
-//   //   int applePrice = 0;
-//   //   if(appleNum == 3){
-//   //     applePrice = 280;
-//   //   }
-//   //   else
-//   //   {
-//   //     applePrice = unit_apple_Price * appleNum;
-//   //   }
-//   //   return applePrice;
-//   // }
-//   // three_Set_Num =Math.ceil(appleNum/3);
-//   // return three_Set_Num;
