7c823fee5939066589f6a1034d406cc973988b23
Sun Yifan <yifansun0125@gmail.com>
2021-06-11 10:57:27 +0900

    [clean]TaxCalculator,TaxCalculator,TaxCalculatorTestのindent整理

--- lib/src/main/java/junit5/Supermarket/impl/TaxCalculatorImpl.java
+++ lib/src/main/java/junit5/Supermarket/impl/TaxCalculatorImpl.java
@@ -9,21 +9,21 @@ import junit5.Supermarket.Item;
 
 public class TaxCalculatorImpl implements TaxCalculator {
 
-    @Override
-    public BigDecimal countPriceWithTax(int id, int itemNum) {
-        double taxRate = 0.08;
-        if (id == Item.TOBACCO.getId() || id == Item.MENTHOLTOBACCO.getId()) {
-            taxRate = 0;//タバコなら税率0にする
-        }
-        int itemPrice = 0;
-        for (Item item : Item.values()) {
-            if (item.getId() == id) {
-                itemPrice = item.getPrice();
-                break;
-            }
-        }
-        BigDecimal discountAmount_decimal = new BigDecimal(taxRate * itemPrice * itemNum);//割引金額計算
-        BigDecimal discountAmount = discountAmount_decimal.setScale(2, RoundingMode.HALF_UP);//割引金額小数二位まで保留
-        return (discountAmount);
+  @Override
+  public BigDecimal countPriceWithTax(int id, int itemNum) {
+    double taxRate = 0.08;
+    if (id == Item.TOBACCO.getId() || id == Item.MENTHOLTOBACCO.getId()) {
+      taxRate = 0;//タバコなら税率0にする
     }
+    int itemPrice = 0;
+    for (Item item : Item.values()) {
+      if (item.getId() == id) {
+        itemPrice = item.getPrice();
+        break;
+      }
+    }
+    BigDecimal discountAmount_decimal = new BigDecimal(taxRate * itemPrice * itemNum);//割引金額計算
+    BigDecimal discountAmount = discountAmount_decimal.setScale(2, RoundingMode.HALF_UP);//割引金額小数二位まで保留
+    return (discountAmount);
+  }
 }
--- lib/src/test/java/junit5/Supermarket/TaxCalculatorTest.java
+++ lib/src/test/java/junit5/Supermarket/TaxCalculatorTest.java
@@ -11,28 +11,28 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("消費税計算test")
 class TaxCalculatorTest {
 
-    private TaxCalculator t;
+  private TaxCalculator t;
 
-    @BeforeEach
-    void 準備() {
-        t = new TaxCalculatorImpl();
+  @BeforeEach
+  void 準備() {
+    t = new TaxCalculatorImpl();
+  }
+
+  @Nested
+  class 番号と個数を渡すと合計消費税を返す {
+    @Test
+    void みかん３を渡すと割引金額9_60を返す() {
+      BigDecimal d = new BigDecimal("9.60");
+      assertEquals(d, t.countPriceWithTax(2, 3));
     }
 
-    @Nested
-    class 番号と個数を渡すと合計消費税を返す {
-        @Test
-        void みかん３を渡すと割引金額9_60を返す() {
-            BigDecimal d = new BigDecimal("9.60");
-            assertEquals(d, t.countPriceWithTax(2, 3));
-        }
-
-        @Test
-        void タバコ何個渡しても消費税0() {
-            BigDecimal d = new BigDecimal("0.00");
-            assertEquals(d, t.countPriceWithTax(7, 3));
-        }
-
+    @Test
+    void タバコ何個渡しても消費税0() {
+      BigDecimal d = new BigDecimal("0.00");
+      assertEquals(d, t.countPriceWithTax(7, 3));
     }
+
+  }
 
 
 }
